95de514e0ad5ea5510142eda17956baa13e584bb
Taboga Marc <taboga.marc@gmail.com>
2017-08-30 11:22:58 +0200

    Add bdd

--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,8 @@ def home(request):
 
     conn = sqlite3.connect('db.sqlite3')
     c = conn.cursor()
-    #c.execute('''CREATE TABLE stocks
-     #            (date text, trans text, symbol text, qty real, price real)''')
+    c.execute('''CREATE TABLE stocks
+                    (date text, trans text, symbol text, qty real, price real)''')
 
     # Insert a row of data
     c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
