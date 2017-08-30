3af01d74fe22903eb9edebdf5ea78ef46aae28eb
Taboga Marc <taboga.marc@gmail.com>
2017-08-30 11:15:23 +0200

    Add bdd

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
-
+import sqlite3
 from django.http import HttpResponse
 
 def home(request):
     device = request.META
+
+    conn = sqlite3.connect('db.sqlite3')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE stocks
+                 (date text, trans text, symbol text, qty real, price real)''')
+
+    # Insert a row of data
+    c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+    c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY2','RHAT',100,35.14)")
+
+    # Save (commit) the changes
+    conn.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
+    conn = sqlite3.connect('db.sqlite3')
+    c = conn.cursor()
+    c.execute("SELECT * FROM stocks")
+
+    print('res:')
+    print(c.fetchall())
     """ Exemple de page HTML, non valide pour que l'exemple soit concis """
     text = """<h1>Bienvenue sur mon blog !</h1>
               <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
-    return render(request, "page.html")
+    return HttpResponse(str(c.fetchall()))
