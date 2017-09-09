6ed653fc31c29a6c1c7aa690ca1ccab81a3040c3
Taboga Marc <taboga.marc@gmail.com>
2017-09-09 16:38:43 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,45 @@
 from django.shortcuts import render
 import sqlite3
 from django.http import HttpResponse
+import psycopg2
+import sys
+import pprint
+
+
 
 def home(request):
     device = request.META
 
-    conn = sqlite3.connect('db.sqlite3')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE stocks
-                    (date text, trans text, symbol text, qty real, price real)''')
+    conn_string = "host='ec2-107-22-167-179.compute-1.amazonaws.com' dbname='decjkd37coqf12' user='yfzfluxnsnhmrc' password='b57511fa3f7b247bf18c74a0c7fb36d9eb688bafbe4ccb37c401e1143f3d6032'"
 
-    # Insert a row of data
-    c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-    c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY2','RHAT',100,35.14)")
+    # print the connection string we will use to connect
+    print("Connecting to database\n	->%s" % (conn_string))
 
-    # Save (commit) the changes
+    # get a connection, if a connect cannot be made an exception will be raised here
+    print('before')
+    conn = psycopg2.connect(conn_string)
+    print('after')
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+    print(cursor)
+    print('after_cursor')
+    cursor.execute("""CREATE TABLE temps (
+        ville           varchar(80),
+        t_basse         int,           -- température basse
+        t_haute         int,           -- température haute
+        prcp            real,          -- précipitation
+        date            date
+    );""")
+    print('after_execute create')
+
+    cursor.execute("""INSERT INTO temps VALUES ('San Francisco', 46, 50, 0.25, '1994-11-27');""")
+    cursor.execute("""INSERT INTO temps VALUES ('San Francisco2', 46, 50, 0.25, '1994-11-27');""")
+
+    cursor.execute("""SELECT * FROM temps;""")
+    res = cursor.fetchall()
+    cursor.close()
+    # commit the changes
     conn.commit()
 
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
-    conn.close()
-    conn = sqlite3.connect('db.sqlite3')
-    c = conn.cursor()
-    c.execute("SELECT * FROM stocks")
 
-    print('res:')
-    a= c.fetchall()
-    print(a)
-    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
-    text = """<h1>Bienvenue sur mon blog !</h1>
-              <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
-    return HttpResponse(a)
+    return HttpResponse(res)
