c86803e66cafce51f9f6e2297cc63e1f3c45ffd2
Taboga Marc <taboga.marc@gmail.com>
2017-09-18 18:58:52 +0200

    add form

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^process$', views.process, name='process'),
     url(r'^accueil$', views.home),
+    url(r'^contact/$', views.contact, name='contact')
 ]
